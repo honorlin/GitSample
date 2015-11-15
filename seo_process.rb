74bff2b0b0320fb3099d32db41e3a5eb9b3ca0c9
Tenhon Lin <tenhon.lin@gmail.com>
2015-11-15 10:16:15 +0800

    finish by b

--- seo_process.rb
+++ seo_process.rb
@@ -7,7 +7,8 @@ class seo
 
   "do something by a ......"
 
-  "do something by b, modify by b,a, modify by a ... ......"
+  "add something by b ......ccc
+  ,do something by b, modify by b,a, modify by a ... ......"
 
 
 end
